5b872e3588f514afd6a1ca6e950409835b91bbc4
Saumya Sakshi <saumyasakshi41@gmail.com>
2026-01-12 15:10:15 +0530

    Initial commit

---
+++ Microservices/Microservice/quizapp/src/test/java/com/somyu/quizapp/QuizappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somyu.quizapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
